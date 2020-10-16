0c055a0954d03e18961d91ce22c0b8ec7a4e77b5
cypher-manit <72973685+cypher-manit@users.noreply.github.com>
2020-10-16 17:17:25 +0530

    Update LCM^k.cpp

--- LCM^k.cpp
+++ LCM^k.cpp
@@ -1,31 +1,8 @@
 /*
-from HackerEarch july circuit long challange. 
-
-QUESTION:
-Given three integers N,M,K and array of integers a1,a2,...,an.
-Find the value of LCM(a1k,a2k,...,ank) mod M.
-
-Input:
-The first line of the input contains a single integer T denoting the number of test cases(1 ≤ T ≤ 10). 
-The description of T test cases follows.
-First line contains three positive integers N, M, K(1 ≤ N ≤ 3 * 105, 1 ≤ M, K ≤ 109).
-The second line contains N space-separated integers a1,a2,...,an.(1 ≤ ai ≤ 106,1 ≤ i ≤ N).
-
-Output:
-For each test case print the value of LCM(a1k,a2k,...,ank) mod M.
-
-
-sample input:
-1
-5 20 3
-17 2 9 4 12
-
-sample output:
-8
-
+this is a solution for CP:
+write a function for prime no quries
 */
 
-// Write your code here
 #include <bits/stdc++.h>
 #define MAXN   1000001 
 typedef long long ll;
@@ -35,25 +12,6 @@ int spf[MAXN];
 
 ll powmod(ll a,ll b, ll mod) {ll res=1;a%=mod; assert(b>=0); for(;b;b>>=1){if(b&1)res=res*a%mod;a=a*a%mod;}return res;}
 
-  
-
-void sieve() 
-{ 
-    spf[1] = 1; 
-    for (int i=2; i<MAXN; i++)  
-        spf[i] = i; 
-    for (int i=4; i<MAXN; i+=2) 
-        spf[i] = 2; 
-  
-    for (int i=3; i*i<MAXN; i++) { 
-
-        if (spf[i] == i) { 
-            for (int j=i*i; j<MAXN; j+=i) 
-                if (spf[j]==j) 
-                    spf[j] = i; 
-        } 
-    } 
-} 
 
 map<int, int> getFactorization(int x) { 
     map<int,int> ret; 
